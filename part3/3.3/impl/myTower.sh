ec064209797117d93cba869e7335e7da5ad74ffb
chenhuiz <chenhui.zhao@helsinki.fi>
2020-05-08 01:27:14 +0300

    3.2-3.3

---
+++ part3/3.3/impl/myTower.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+### Download Git ###
+apt-get update
+apt-get install -y git
+
+### Download docker engin ###
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+### Clone repo ####
+echo "Enter a path of the git repository for cloning:"
+read git_path
+echo "Set a directory to clone to:"
+read dir
+git clone $git_path $dir
+
+### Make Dockerfile ####
+cd $dir
+rm Dockerfile
+echo -e "FROM node \nWORKDIR /mydir \nCOPY . . \nRUN npm install \nCMD ["npm", "start"]" >> Dockerfile
+
+### Publish to Docker Hub ####
+echo "Name your project docker image:"
+read repo_name
+echo "Enter your Docker Hub username:"
+read username
+
+docker build -t $repo_name .
+docker login
+docker push $username/$repo_name
